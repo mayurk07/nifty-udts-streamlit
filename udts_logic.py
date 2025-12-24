a85b882a8f6146a5eee9b5142522bcbfbf7f33cc
mayurk07 <mayurk07@users.noreply.github.com>
2025-12-24 16:51:27 +0530

    Update udts_logic.py

--- udts_logic.py
+++ udts_logic.py
@@ -1,8 +1,1 @@
-def compute_udts(open_series, close_series):
-last_open = float(open_series.values[-1])
-last_close = float(close_series.values[-1])
-
-if last_close > last_open:
-return "Up"
-else:
-return "Down"
+# UDTS logic will go here
