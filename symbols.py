e42d89168d0539ab15c58502bf751dbee3d00195
mayurk07 <mayurk07@users.noreply.github.com>
2025-12-24 16:27:38 +0530

    Update symbols.py

--- symbols.py
+++ symbols.py
@@ -1,1 +1,5 @@
-
+NSE_100 = [
+    "RELIANCE.NS", "TCS.NS", "INFY.NS", "HDFCBANK.NS", "ICICIBANK.NS",
+    "LT.NS", "SBIN.NS", "HINDUNILVR.NS", "ITC.NS", "AXISBANK.NS",
+    # you can extend this to full NSE 100 later
+]
