8ac3776c19d6379753ef8c230a531c70963e8a04
mayurk07 <mayurk07@users.noreply.github.com>
2025-12-24 16:29:30 +0530

    Update udts_logic.py

--- udts_logic.py
+++ udts_logic.py
@@ -1,1 +1,10 @@
-
+def compute_udts(open_series, close_series):
+    """
+    Placeholder.
+    Will implement full G1-R1 / G2-R2 logic next.
+    """
+    if close_series.iloc[-1] > open_series.iloc[-1]:
+        return "UP"
+    elif close_series.iloc[-1] < open_series.iloc[-1]:
+        return "DOWN"
+    return "NEUTRAL"
